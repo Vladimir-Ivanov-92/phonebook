62e6704ad4abf0907403d773a600b627bdaa5559
vova2092 <vovai2092@yandex.ru>
2023-08-26 23:06:31 +0300

    Refactoring:  find_a_record.py

--- phonebook/handlers/find_a_record.py
+++ phonebook/handlers/find_a_record.py
@@ -1,9 +1,13 @@
+from typing import Optional
+
 import pandas as pd
+from pandas import DataFrame
 
 from phonebook.service import _get_dataframe, _get_column_names_from_df
 
 
 class FindRecord():
+    """Поиск записи в телефонной книге"""
 
     def __init__(self, table_name):
         self.table_name = table_name
@@ -11,6 +15,7 @@ class FindRecord():
         self.num_rows = self.df.shape[0]  # Количество строк в DataFrame
 
     def _formatted_print(self, result_search):
+        """Отформатированный вывод в консоль полученных данных"""
         name_columns = _get_column_names_from_df(self.table_name)
         print("Результаты поиска: ")
         print(name_columns)
@@ -23,20 +28,26 @@ class FindRecord():
             )
             print(formatted_row)
 
-    def index_search(self, index):
+    def index_search(self, index: int) -> Optional[DataFrame]:
+        """Поиск строки по индексу(номеру)"""
         if index >= 0 and index < self.num_rows:
             result_search = pd.DataFrame([self.df.iloc[index]])
+            # Вывод результа поиска в терминал
             self._formatted_print(result_search)
             return result_search
         else:
             print("Индекс за пределами диапазона")
 
-    def condition_search(self, column: str, condition: str):
+    def condition_search(self, column: str, condition: str) -> Optional[DataFrame]:
+        """Поиск строки по одному условию"""
         result_search = self.df[self.df[column] == condition]
+        # Вывод результа поиска в терминал
         self._formatted_print(result_search)
         return result_search
 
-    def conditions_search(self, conditions: list[tuple[str, str]]):
+    def conditions_search(self, conditions: list[tuple[str, str]]) -> Optional[
+        DataFrame]:
+        """Поиск строки по одному или нескольким условиям"""
         # Комбинирование условий с использованием оператора ИЛИ (&)
         combined_condition = self.df[conditions[0][0]] == conditions[0][1]
         for col, value in conditions[1:]:
@@ -48,5 +59,6 @@ class FindRecord():
         if result_search.empty:
             print(f"Совпадений с условием: {conditions} не найдено!")
         else:
+            # Вывод результа поиска в терминал
             self._formatted_print(result_search)
             return result_search

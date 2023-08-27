b3034f280fcaed1c3e05dc52f3d4515cde0cd185
vova2092 <vovai2092@yandex.ru>
2023-08-27 04:58:56 +0300

    Added check of the number of received records for data update.

--- phonebook/menu/handlers_menu.py
+++ phonebook/menu/handlers_menu.py
@@ -64,8 +64,13 @@ def handle_edit_menu(phonebook_entry: PhonebookEntry,
                 column, data = get_data_from_one_condition_menu(
                     number_one_condition_menu)
                 result_search = search_entry.condition_search(column, data)
-                column, data = get_data_from_update_menu()
-                phonebook_entry.edit_entry(result_search, (column, data))
+                if len(result_search) > 1:
+                    print()
+                    print("Выбрано более одного значения. Воспользуйтесь поиском с "
+                          "несколькими условиями, чтоб выбрать одно значение!")
+                else:
+                    column, data = get_data_from_update_menu()
+                    phonebook_entry.edit_entry(result_search, (column, data))
             except Exception:
                 pass
         elif user_input == "3":
@@ -82,9 +87,14 @@ def handle_conditions_edit(phonebook_entry: PhonebookEntry,
             break
         elif number_many_condition_menu == "7":
             result_search = search_entry.conditions_search(conditions)
-            column, data = get_data_from_update_menu()
-            phonebook_entry.edit_entry(result_search, (column, data))
-            break
+            if len(result_search) > 1:
+                print()
+                print("Выбрано более одного значения. Воспользуйтесь поиском с "
+                      "дополнительными условиями, чтоб выбрать одно значение!")
+            else:
+                column, data = get_data_from_update_menu()
+                phonebook_entry.edit_entry(result_search, (column, data))
+                break
         else:
             column, data = get_data_from_many_condition_menu(
                 number_many_condition_menu)
